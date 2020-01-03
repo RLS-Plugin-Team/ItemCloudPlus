cfb47e3c1e04fddd3db621113b670c87e2c92ed6
narapon <43005911+narapons@users.noreply.github.com>
2020-01-03 22:29:31 +0900

    Update MainClass.php

--- src/onebone/itemcloud/MainClass.php
+++ src/onebone/itemcloud/MainClass.php
@@ -202,7 +202,7 @@ class MainClass extends PluginBase implements Listener{
 							break;
 						}
 						
-						if(!$this->getServer()->getPlayerExact($params[0])->isOnline()){
+						if($this->getServer()->getPlayerExact($params[0])->isOnline() == NULL){
 							$sender->sendMessage("[ItemCloud] §c{$params[0]}はオフラインです");
 							break;
 						}
